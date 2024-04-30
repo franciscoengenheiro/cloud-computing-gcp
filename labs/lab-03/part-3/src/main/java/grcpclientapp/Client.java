e1270484b3bfad8daf9af2d4e829194e9a877f7d
Gonçalo Frutuoso <goncasantacruz@gmail.com>
2024-04-30 22:33:41 +0100

    added publishMessage

--- labs/lab-03/part-3/src/main/java/grcpclientapp/Client.java
+++ labs/lab-03/part-3/src/main/java/grcpclientapp/Client.java
@@ -5,6 +5,7 @@ import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.protobuf.Empty;
 import forum.ExistingTopics;
 import forum.ForumGrpc;
 import forum.ForumMessage;
@@ -64,6 +65,9 @@ public class Client {
         };
         noBlockStub.topicSubscribe(request, responseObserver);
 
+        // Publish a message
+        publishMessage();
+
         // Keep the main thread alive
         while (true) {
         }
@@ -74,11 +78,11 @@ public class Client {
         BlobId blobId = BlobId.of(bucketName, blobName);
         Storage storage = StorageOptions.getDefaultInstance().getService();
         Blob blob = storage.get(blobId);
-        String downloadTo = absFileName + "/" + blobName;
+        String downloadTo = absFileName + "/" + blobName + ".png";
 
         try (OutputStream writeTo = new FileOutputStream(downloadTo)) {
             if (blob.getSize() < 1_000_000) {
-                // Blob is small read all blob content in one request
+                // Blob is small to read all blob content in one request
                 blob.downloadTo(writeTo);
             } else {
                 // If Blob size is big, use a blob's channel reader
@@ -97,5 +101,32 @@ public class Client {
         } catch (Exception e) {
             System.out.println("Error downloading blob: " + e.getMessage());
         }
+    }
+
+    private static void publishMessage() {
+        // Create a message
+        ForumMessage message = ForumMessage.newBuilder()
+                .setFromUser("username")
+                .setTopicName("topic")
+                .setTxtMsg("message;lab3-bucket-g04-europe;lab3-blob-g04-europe")
+                .build();
+
+        // Publish the message
+        noBlockStub.publishMessage(message, new StreamObserver<>() {
+            @Override
+            public void onNext(Empty empty) {
+                System.out.println("Message published");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Error: " + throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Publishing completed");
+            }
+        });
     }
 }
